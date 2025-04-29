f6d7d949edd4061e769f86640bb80c1d8dcb771d
Ivan Borshchov <ivan@devforth.io>
2025-04-29 07:44:10 +0000

    fix: recover context application for upload plugin

--- types.ts
+++ types.ts
@@ -120,6 +120,12 @@ export type PluginOptions = {
 
 
     /**
+     * Fields for conetext which will be used to generate the image.
+     * If specified, the plugin will use fields from the record to provide additional context to the AI model.
+     */
+    fieldsForContext?: string[],
+
+    /**
      * The number of images to generate
      * in one request
      */
