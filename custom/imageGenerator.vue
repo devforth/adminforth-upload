a81e1af96471e5d71feab977124a7cc24ec21422
yaroslav8765 <pechorkin2014@gmail.com>
2025-12-12 14:29:31 +0200

    fix: stop generation of image when generation popup is closed

--- custom/imageGenerator.vue
+++ custom/imageGenerator.vue
@@ -11,7 +11,7 @@
                     {{ $t('Generate image with AI') }}
                 </h3>
                 <button type="button" 
-                  @click="emit('close')"
+                  @click="() => {stopGeneration = true; emit('close')}"
                   class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" >
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -161,7 +161,7 @@
                   disabled:opacity-50 disabled:cursor-not-allowed"
                 >{{ $t('Use image') }}</button>
                 <button type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  @click="emit('close')"
+                  @click="() => {stopGeneration = true; emit('close')}"
                 >{{ $t('Cancel') }}</button>
             </div>
         </div>
@@ -190,6 +190,7 @@ const props = defineProps(['meta', 'record']);
 const images = ref([]);
 const loading = ref(false);
 const attachmentFiles = ref<string[]>([])
+const stopGeneration = ref(false);
 
 function minifyField(field: string): string {
   if (field.length > 100) {
@@ -390,7 +391,7 @@ async function generateImages() {
       }
     }
     await new Promise((resolve) => setTimeout(resolve, 2000));
-  } while (jobStatus === 'in_progress' || jobStatus === null)
+  } while ((jobStatus === 'in_progress' || jobStatus === null) && !stopGeneration.value);
   
   if (error) {
       adminforth.alert({
