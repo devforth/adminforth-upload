a31104244e4e81191dc673ccf382fa07e1bb0378
Pavlo Kulyk <pkulyk@devforth.io>
2026-06-09 13:40:58 +0300

    fix: - replace custom spinner to afcl - update spinner visual - add condition for disable upload btn in modal

--- custom/imageGenerator.vue
+++ custom/imageGenerator.vue
@@ -54,7 +54,7 @@
               </div>
             </div>
             <input ref="fileInput" class="hidden" type="file" @change="handleAddFile" accept="image/*" />
-            <button v-if="!uploading" @click="fileInput?.click()" type="button" class="relative group hover:border-gray-400 transition border-gray-300 flex items-center justify-center w-14 h-14 border-2 border-dashed rounded-lg">
+            <button v-if="!uploading" @click="fileInput?.click()" type="button" :disabled="requestAttachmentFilesUrls.length >= 1" class=" upload-btn relative group hover:border-gray-400 disabled:opacity-40 disabled:cursor-not-allowed transition border-gray-300 flex items-center justify-center w-14 h-14 border-2 border-dashed rounded-lg">
               <div class="flex flex-col items-center justify-center gap-1">
                 <IconCloseOutline class="group-hover:text-gray-500 transition rotate-45 w-5 h-5 text-gray-300" />
                 <p class="text-gray-300 group-hover:text-gray-500 transition text-[10px] leading-none">Ctrl+v</p>
@@ -90,15 +90,11 @@
 
               <div 
                 v-if="loading"
-                class=" absolute flex items-center justify-center inset-0 z-40 bg-white/80 dark:bg-gray-900/80 rounded-xl">
-                <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 50.5908Z" fill="currentColor"/>
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                </svg>
-                <span class="sr-only">{{ $t('Loading...') }}</span>
+                class="spinner absolute flex items-center justify-center inset-0 z-40 rounded-xl">
+                <Spinner class="w-8 h-8" />
               </div>
-              <div v-if="loadingTimer" class="absolute inset-0 pt-12 z-40 flex items-center justify-center bg-white/80 dark:bg-gray-900/80 rounded-xl">
-                <div class="text-gray-800 dark:text-gray-100 text-base font-semibold" v-if="!historicalAverage">
+              <div v-if="loadingTimer" class=" spinner-description absolute inset-0 z-40 flex items-center justify-center">
+                <div class="text-gray-800 dark:text-gray-100 text-base font-semibold mt-20" v-if="!historicalAverage">
                   {{ formatTime(loadingTimer) }} {{ $t('passed...') }}
                 </div>
                 <div class="w-48" v-else>
@@ -129,7 +125,7 @@
                     />
                   </div>
                   <div v-if="images.length === 0" class="w-full h-full flex items-center justify-center border-2 border-dashed border-gray-200 dark:border-gray-600 rounded-xl bg-gray-50 dark:bg-gray-700">
-                    <span class="text-gray-400 dark:text-gray-500 text-sm">{{ $t('Your generated image will appear here') }}</span>
+                    <span v-if="!loadingTimer" class="text-gray-400 dark:text-gray-500 text-sm">{{ $t('Your generated image will appear here') }}</span>
                   </div>
                 </div>
               </div>
@@ -212,7 +208,7 @@ import adminforth from '@/adminforth';
 import { ProgressBar, Button } from '@/afcl';
 import * as Handlebars from 'handlebars';
 import { IconCloseOutline } from '@iconify-prerendered/vue-flowbite';
-import { Tooltip, Skeleton } from '@/afcl'
+import { Tooltip, Skeleton, Spinner } from '@/afcl'
 import { useRoute } from 'vue-router';
 
 const { t: $t, t } = useI18n();
